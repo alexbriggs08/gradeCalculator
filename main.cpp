bbc6906c5a19dcb037a65d0edf86f021cf95ec8f
alex <alexbr1208@gmail.com>
2024-10-18 14:34:27 -0400

    Initial commit -- help me!!

---
+++ main.cpp
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <thread>
+#include <chrono>
+
+
+using namespace std::this_thread;
+
+int main() {
+    // variables
+    int numberOfInputs = 0;
+    int gradeInputs;
+    int avgGrade = 0;
+    char complete;
+    bool inputsComplete = false;
+
+    std::cout << "Enter in you're grade: ";
+    std::cin >> gradeInputs; ++ numberOfInputs; // gets the grade input and adds 1 to the number of inputs
+    std::cout << numberOfInputs <<std::endl; // displays the number of inputs temp
+    avgGrade = (gradeInputs / numberOfInputs);
+    std::cout << "Enter in you're grade: ";
+    std::cin >> gradeInputs; ++ numberOfInputs;
+    std::cout << numberOfInputs <<std::endl;
+    avgGrade = (gradeInputs / numberOfInputs);
+    std::cout << "Are you complete?" << std::endl;
+    std::cin >> complete;
+
+    if (complete == 'y' || complete == 'Y') {
+
+        inputsComplete = true;
+
+    }
+
+
+if (inputsComplete) {
+
+    std::cout << avgGrade << std::endl;
+
+} else {
+
+    std::cout << "Enter in you're grade: ";
+    std::cin >> gradeInputs; ++ numberOfInputs; // gets the grade input and adds 1 to the number of inputs
+    std::cout << numberOfInputs <<std::endl; // displays the number of inputs temp
+    avgGrade = (gradeInputs / numberOfInputs);
+    std::cout << "Are you complete?" << std::endl;
+
+}
+
+    std::cout << "You're Average Grade Is: " << avgGrade;
+
+
+    return 0;
+}
