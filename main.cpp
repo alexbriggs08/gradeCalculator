9c67689bd34e8134c0cc2112a446ab08c5e78470
alex <alexbr1208@gmail.com>
2024-10-18 16:40:28 -0400

    -- made it work -- added for the first one it will not accept a number that is not between 1-100   -need to fix the second one so it will do that

--- main.cpp
+++ main.cpp
@@ -1,51 +1,57 @@
 #include <iostream>
 #include <thread>
-#include <vector> // for calculations like avg, exponents, ect...
+#include <cmath> // for calculations like avg, exponents, ect...
 
 
 int main() {
     // variables
-    int numberOfInputs = 0;
-    int gradeInputs;
-    int avgGrade = 0;
+    double numberOfInputs = 0;
+    double gradeInputs =0;
+    double sumGrade = 0;
+    double avgGrade = 0;
     char complete;
     bool inputsComplete = false;
 
-    std::cout << "Enter in you're grade: ";
-    std::cin >> gradeInputs; ++ numberOfInputs; // gets the grade input and adds 1 to the number of inputs
-    std::cout << numberOfInputs <<std::endl; // displays the number of inputs temp
-    avgGrade = (gradeInputs / numberOfInputs);
-    // copied ^
-    std::cout << "Enter in you're grade: ";
-    std::cin >> gradeInputs; ++ numberOfInputs;
-    std::cout << numberOfInputs <<std::endl;
-    avgGrade = (gradeInputs / numberOfInputs);
 
-    std::cout << "Are you complete?" << std::endl;
-    std::cin >> complete;
+    std::cout << "Enter in you're grade: " << std::endl;
+    std::cin >> gradeInputs;// gets the grade input and adds 1 to the number of inputs
+    // a if then statement to make sure the number given is between 0 - 100
+    if (gradeInputs >= 0 || gradeInputs <= 100) {
+        ++ numberOfInputs;
+        sumGrade = gradeInputs + sumGrade;
+        avgGrade = (sumGrade / numberOfInputs);
 
-    if (complete == 'y' || complete == 'Y') {
-
-        inputsComplete = true;
-
+    } else {
+        // this is just a message to send to the person if the number they gave is not between 0 - 100
+        std::cout << "Grade must be between 0 and 100" << std::endl;
     }
 
 
-if (inputsComplete) {
+    while (inputsComplete == false) {
 
-    std::cout << avgGrade << std::endl;
+        std::cout << "Enter in you're grade: " << std::endl;
+        std::cin >> gradeInputs;
+        // a if then statement to make sure the number given is between 0 - 100
+        if (gradeInputs >= 0 || gradeInputs <= 100) {
+            ++ numberOfInputs;
+            sumGrade = gradeInputs + sumGrade;
+            avgGrade = (sumGrade / numberOfInputs);
 
-} else {
+            std::cout << "Are you complete? (y/n)" << std::endl;
+            std::cin >> complete;
+        } else {
+            // this is just a message to send to the person if the number they gave is not between 0 - 100
+            std::cout << "Grade must be between 0 and 100" << std::endl;
+        }
 
-    std::cout << "Enter in you're grade: ";
-    std::cin >> gradeInputs; ++ numberOfInputs;
-    std::cout << numberOfInputs <<std::endl;
-    avgGrade = (gradeInputs / numberOfInputs);
-    std::cout << "Are you complete?" << std::endl;
 
-}
+        if (complete == 'y' || complete == 'Y') {
 
-    std::cout << "You're Average Grade Is: " << avgGrade;
+            inputsComplete = true;
+
+        }
+    }
+        std::cout << "You're Average Grade Is: " << avgGrade;
 
 
     return 0;
