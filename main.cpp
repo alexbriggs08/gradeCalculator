7ac05fb14760c2de5adcef9a71c555810f3fc329
alex <alexbr1208@gmail.com>
2024-10-18 20:55:56 -0400

    -- 1.3 -- bugs   -still says would you like to continue even if you have input a number not 1-100

--- main.cpp
+++ main.cpp
@@ -13,37 +13,23 @@ int main() {
     bool inputsComplete = false;
 
 
-    std::cout << "Enter in you're grade: " << std::endl;
-    std::cin >> gradeInputs;// gets the grade input and adds 1 to the number of inputs
-    // a if then statement to make sure the number given is between 0 - 100
-    if (gradeInputs >= 0 || gradeInputs <= 100) {
-        ++ numberOfInputs;
-        sumGrade = gradeInputs + sumGrade;
-        avgGrade = (sumGrade / numberOfInputs);
-
-    } else {
-        // this is just a message to send to the person if the number they gave is not between 0 - 100
-        std::cout << "Grade must be between 0 and 100" << std::endl;
-    }
-
-
-    while (inputsComplete == false) {
+    while (not inputsComplete) {
 
         std::cout << "Enter in you're grade: " << std::endl;
         std::cin >> gradeInputs;
         // a if then statement to make sure the number given is between 0 - 100
-        if (gradeInputs >= 0 || gradeInputs <= 100) {
+        if (gradeInputs >= 0 && gradeInputs <= 100) {
             ++ numberOfInputs;
             sumGrade = gradeInputs + sumGrade;
             avgGrade = (sumGrade / numberOfInputs);
 
-            std::cout << "Are you complete? (y/n)" << std::endl;
-            std::cin >> complete;
         } else {
             // this is just a message to send to the person if the number they gave is not between 0 - 100
             std::cout << "Grade must be between 0 and 100" << std::endl;
         }
 
+        std::cout << "Are you finished? (y/n)" << std::endl;
+        std::cin >> complete;
 
         if (complete == 'y' || complete == 'Y') {
 
