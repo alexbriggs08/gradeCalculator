1d27f2384bb94b15bb506281d9e668de2710e131
alex <alexbr1208@gmail.com>
2024-10-21 08:51:00 -0400

    -- 1.4.1 -- added a bugs fix list -- added a message when you only enter 1 grade

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main() {
 
     while (not inputsComplete) {
 
-        std::cout << "Enter in you're grade: " << std::endl;
+        std::cout << "Enter in you're grade: " << '\n';
         std::cin >> gradeInputs;
         // a if then statement to make sure the number given is between 0 - 100
         if (gradeInputs >= 0 && gradeInputs <= 100) {
@@ -24,19 +24,19 @@ int main() {
             avgGrade = (sumGrade / numberOfInputs);
 
 
-            std::cout << "Are you finished? (y/n)" << std::endl;
+            std::cout << "Are you finished? (y/n)" << '\n';
             std::cin >> complete;
 
         } else {
             // this is just a message to send to the person if the number they gave is not between 0 - 100
-            std::cout << "Grade must be between 0 and 100" << std::endl;
+            std::cout << "Grade must be between 0 and 100" << '\n';
         }
 
 
         if (complete == 'y' || complete == 'Y') {
 
             if (numberOfInputs == 1) {
-                std::cout << "Please enter more than one grade: " << std::endl;
+                std::cout << "Please enter more than one grade: " << '\n';
 
             } else {
                 inputsComplete = true;
